b668aac946ae379c9d2491f88d6bfb97a60afd59
charcharmasonjar <charlotte.dibb@gmail.com>
2019-10-16 18:06:16 -0700

    Create queries that use node-postgress
    Queries accept user input from the command line and return relevant data

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,30 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+pool.query(`
+SELECT
+  DISTINCT teachers.name AS teacher,
+  cohorts.name AS cohort
+FROM
+  teachers
+  JOIN assistance_requests ON assistance_requests.teacher_id = teachers.id 
+  JOIN students ON assistance_requests.student_id = students.id 
+  JOIN cohorts on students.cohort_id = cohorts.id
+WHERE
+  cohorts.name LIKE '%${process.argv[2] || 'JUL02'}%'
+ORDER BY
+  teachers.name
+`)
+
+.then(res => {
+  res.rows.forEach(teacher => {
+    console.log(`${teacher.cohort}: ${teacher.teacher}`);
+  })
+})
+.catch(err => console.log('query error', err.stack));
